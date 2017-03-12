5b8eaaabd7c85196d1eb047ed39ef4e931ecb5de
caojiayan <136665305@qq.com>
2017-03-12 16:53:07 +0800

    微信减肥

---
+++ a.gzghwy.com/jianfei/js/cj.js
@@ -0,0 +1,3 @@
+var  parr= ['jj47weixins','jfweixin01s']; 
+var stxlwx=(parr[Math.floor(Math.random()*parr.length)]); 
+        
---
+++ a.gzghwy.com/jianfei/pp/js/cj.js
@@ -0,0 +1,27 @@
+//var arr_wx =['484564195','451941926','941945085','481341915','525419502','940141977']
+//var arr_wx =['20160216','941901896','472951419','474190509','482419095','541981408','744191051','425931419','419273216','384198904','419143110','484195963','656204197','419703295','419731550','419844302']
+//var wx_index = Math.floor((Math.random() * arr_wx.length));
+//stxlwx = arr_wx[wx_index];
+//var img = arr_wx[wx_index]+".jpg";
+//var wx_img = "<img width='280' height='280' src='http://ccc.bmwcar.cn/tianya/"+img+"'>";
+
+		//arr_wx=['s8x8203','s8x8204','s8x8205','s8x8206','jfweixin001','jfweixin008','s8x8043','s8x8044'];
+		
+		//���ڿ���ͼƬ�ȵ���ʾ
+//		var p_arr=[['����ʫ','index_files/img/ssdr2.jpg','ʫʫ',['jj47weixins','jfweixin01s']],
+//				   ['�μ�','index_files/img/ssdr4.jpg','�Ѽ�',['jfweixin01s','jj47weixins']]];
+
+//		var p_index=Math.floor((Math.random()*p_arr.length));
+//		var pList=p_arr[p_index];
+//		var pic1=pList[1];
+//		var name=pList[2];
+//	
+//		arr_wx=pList[3];
+//		var wx_index = Math.floor((Math.random()*arr_wx.length));
+//		var stxlwx = arr_wx[wx_index];
+//		var img = arr_wx[wx_index]+".jpg";
+//		var wx_img = "<img src='index_files/img/"+img+"'>";
+//      var p_arr=['jj47weixins','jfweixin01s'];
+        var  parr= ['jj47weixins','jfweixin01s']; 
+        var stxlwx=(parr[Math.floor(Math.random()*parr.length)]); 
+        
